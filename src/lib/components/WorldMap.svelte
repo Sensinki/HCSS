49be26f347b5e76c8d0bfc55a814e0706376dbab
senanurkorkutan <snnrkrk1@gmail.com>
2025-01-08 16:36:53 +0100

    second section "verdeeldheid is done

--- src/lib/components/WorldMap.svelte
+++ src/lib/components/WorldMap.svelte
@@ -1,20 +1,19 @@
 <script>
-    // export let message = 'Hallo Wereld!';
 </script>
 
-<!-- <p>{message}</p> -->
 <section>
     <img
         src="/images/landen.png"
         alt="World Map"
     />
-    <div class="alignment">
+
+    <section>
         <h1>Titel h1</h1>
         <p>
             Lorem ipsum dolor sit amet consectetur. Pellentesque enim neque nunc amet at. Id ac neque urna sit ornare mattis in. Fames praesent venenatis erat
             commodo habitant eget. Et lorem morbi pellentesque arcu cursus malesuada lacus.
         </p>
-    </div>
+    </section>
 </section>
 
 <style>
@@ -23,14 +22,14 @@
             display: grid;
             justify-items: center;
             align-items: center;
+            padding: 0 2em;
+            background-color: #1a3761;
 
             img {
-                width: 80vw;
                 max-width: 45em;
-                margin: 3em 0;
             }
 
-            .alignment {
+            section {
                 display: flex;
                 flex-direction: column;
                 gap: 0.5em;
